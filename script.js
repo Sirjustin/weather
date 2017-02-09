45e616cbdc63228a7e30120c40770f4a3e04f249
Unknown <Jdavidkahn@gmail.com>
2017-02-08 20:55:54 -0500

    works

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ console.log(api);
     var weatherType = data.weather[0].description;
     var city = data.name;
     var kelvin = data.main.temp;
-    var fahr= kelvin*9/5-459.67 + " F";
-    var celsius = kelvin-273.15 + " C"
+    var fahr= kelvin*9/5-459.67;
+    fahr=Math.floor(fahr)+"F";
+    var celsius = Math.round(kelvin-273.15)+" C";
     var temperature=true;
 
 
@@ -20,17 +21,22 @@ console.log(api);
     $("#weatherType").html(weatherType);
     $("#temp").html(fahr);
     $(".message").html(temp);
+$("#system").html("In Celsius");
+
 
 
   if (kelvin<280){
 $('body').css('background-image', 'url(images/winter.jpg)');
+$("#sweater").html("Leo says throw on a Sweater");
 }
-else{$('body').css('background-image', 'url(images/summer.jpg)');}
+else{$('body').css('background-image', 'url(images/summer.jpg)');
+$("#sweater").html("Leo says birthday suits are ok today");
+}
 
   if (kelvin<280){
-$('#weatherdog').prepend('<img id="winterleo" src="images/wl.jpg" width="200" height="200"/>');
-}
-else{$('#weatherdog').prepend('<img id="winterleo" width="200" height="200"/>');}
+$('#weatherdog').prepend('<img id="winterleo" src="images/wl.jpg"width="200" height="200"/>');}
+
+else{$('#weatherdog').prepend('<img id="summerleo" src="images/sl.jpg" width="200" height="200"/>');}
 
 
 
@@ -40,9 +46,12 @@ else{$('#weatherdog').prepend('<img id="winterleo" width="200" height="200"/>');
      $("#getMessage").click(function(){
 if(temperature===true){
    $("#temp").html(celsius);
+      $("#system").html("In Fahrenheit");
    temperature=false;
 }
 else{ $("#temp").html(fahr);
+  $("#system").html("In Celsius");
+   
 temperature=true;
 
 }
